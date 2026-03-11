35ea97e4c2628bc1dfd0065e97ad37c610532877
Pragatheesh <142164499+Pragatheesh-I@users.noreply.github.com>
2026-03-11 07:08:32 +0530

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -2,7 +2,7 @@ import { loadSecrets } from "./src/lib/loadSecrets.js"
 
 async function start() {
   await loadSecrets()
-
+  process.argv.push("start")  // tells next to run `next start`
   await import("next/dist/bin/next")
 }
 
