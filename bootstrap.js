2f2eac100e6e65fc4c281ba3d5f4796b4ae9541d
Pragatheesh <142164499+Pragatheesh-I@users.noreply.github.com>
2026-03-11 07:23:47 +0530

    Update bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -2,6 +2,10 @@ import { loadSecrets } from "./src/lib/loadSecrets.js"
 
 async function start() {
   await loadSecrets()
+    // Run migrations before starting the app
+  console.log("Running database migrations...")
+  execSync("npx prisma migrate deploy", { stdio: "inherit" })
+  console.log("Migrations complete.")
   process.argv.push("start")  // tells next to run `next start`
   await import("next/dist/bin/next")
 }
